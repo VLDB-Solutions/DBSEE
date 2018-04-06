6a064e964a85beeadda77e030d23e607bf387dfb
mtimothy-vldb <37292580+mtimothy-vldb@users.noreply.github.com>
2018-04-06 13:31:00 +0100

    Update and rename dbsee_tableindexstats_export.sql to dbsee_tableindexstats.sql

--- dbsee_tableindexstats.sql
+++ dbsee_tableindexstats.sql
@@ -5,17 +5,6 @@
 /* Description : Generate CSV to report on tables with missing or    */
 /*               out of date index stats within the Teradata System  */
 /*********************************************************************/
-/* 1 - Export contents of the target table to CSV                    */
-/*********************************************************************/
-
--- export location defined by controlling shell
-
-.set recordmode off
-.set separator '|'
-.set width 2000
-.set titledashes off
-.set null as ' '
-
 select t1.databasename as database_name
       ,t1.tablename    as table_name
       ,t1.columnname   as column_name
@@ -28,7 +17,7 @@ select t1.databasename as database_name
       ,max(t2.lastcollecttimestamp) as last_collect_timestamp
       ,t3.total_perm     as total_perm
       ,current_date - cast(last_collect_timestamp as date) as days_since_collection
-from   dbc.indicesv   t1
+from   dbc.indicesv t1
 left outer join
        dbc.indexstatsv t2
 on     t1.databasename = t2.databasename
@@ -43,8 +32,4 @@ on     t1.databasename = t3.databasename
 and    t1.tablename    = t3.tablename
 group by 1,2,3,4,6
 ;
-.if errorcode <> 0 then .quit errorcode
-
-.logoff
-.exit
 
