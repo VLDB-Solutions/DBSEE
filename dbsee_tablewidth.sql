4751f5b254b6448fea95fe58fc4c7e62cc80f8f1
mtimothy-vldb <37292580+mtimothy-vldb@users.noreply.github.com>
2018-04-06 14:12:02 +0100

    Update and rename dbsee_tablewidth_export.sql to dbsee_tablewidth.sql

--- dbsee_tablewidth.sql
+++ dbsee_tablewidth.sql
@@ -2,20 +2,8 @@
 /* Author      : VLDB                                                */
 /* Date        : 23/02/2017                                          */
 /* Version     : 1                                                   */
-/* Description : Generate csv for reporting on number of columns per */
-/*               table on the Teradata System                        */
+/* Description : Generate report on number of columns per table      */
 /*********************************************************************/
-/* 1 - Export contents of the target table to CSV                    */
-/*********************************************************************/
-
--- export location defined by controlling shell
-
-.set recordmode off
-.set separator '|'
-.set width 2000
-.set titledashes off
-.set null as ' '
-
 select  t1.databasename      as database_name
        ,t1.tablename         as table_name
        ,t2.total             as total_perm
@@ -26,7 +14,7 @@ left join
                ,tablename
                ,sum(currentperm) as total
         from    dbc.tablesizev
-        group by 1,2)t2
+        group by 1,2) t2
 on      t1.databasename = t2.databasename
 and     t1.tablename    = t2.tablename
 inner join 
@@ -36,8 +24,4 @@ and     t1.tablename    = t3.tablename
 where   t3.tablekind in ('O','T')
 group by 1,2,3
 ;
-.if errorcode <> 0 then .quit errorcode
-
-.logoff
-.exit
 
