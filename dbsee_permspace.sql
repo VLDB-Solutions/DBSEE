3d1e8337cf0340551b49b39b0220ef63cdf89b2e
mtimothy-vldb <37292580+mtimothy-vldb@users.noreply.github.com>
2018-04-06 13:28:40 +0100

    Update and rename dbsee_permspace_export.sql to dbsee_permspace.sql

--- dbsee_permspace.sql
+++ dbsee_permspace.sql
@@ -5,17 +5,6 @@
 /* Description : Generate a CSV reporting on available space within  */
 /*               each database on the Teradata System                */
 /*********************************************************************/
-/* 1 - Export contents of the target table to CSV                    */
-/*********************************************************************/
-
--- export location defined by controlling shell
-
-.set recordmode off
-.set separator '|'
-.set width 2000
-.set titledashes off
-.set null as ' '
-
 select  t1.databasename  as database_name
        ,sum(maxperm)     as perm_size
        ,sum(currentperm) as perm_used
@@ -23,12 +12,7 @@ select  t1.databasename  as database_name
        ,cast(case when perm_size = 0 then 0
                   else (cast(perm_free as decimal(25,4))/cast(perm_size as decimal(25,4)))*100
              end as decimal(23,2)) as perm_free_perc
-from    dbc.diskspaceV t1
+from    dbc.diskspaceV
 group by 1
 ;
-.if errorcode <> 0 then .quit errorcode
-
-.logoff
-.exit
-
 
